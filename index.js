ab61334143db6b8cdebb202874f0a3166115ac9e
ntsum <natsukajiwaram@gmail.com>
2019-04-25 13:58:40 -0400

    fdkiuhfoa

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 //reads .env and merges it into process.env
 
-const server = require('./api/server.js');
+const router = require('./api/router.js');
 
 const port = process.env.PORT || 4000;
-server.listen(port, () => {
+router.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
